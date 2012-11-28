2ab5bcaf053cd3c9b3f45de2987a21c8bc28997c
Kevin Ewing <notmyuwmail@gmail.com>
2012-11-27 22:15:59 -0800

    Doing what I can to reduce the shame of upcoming code review

--- js/js.js
+++ js/js.js
@@ -1,14 +1,15 @@
-(
-$.getJSON('lists/test.json', function(data) {
-  var items = [];
+(function() {
+	$.getJSON('lists/test.json', function(data) {
+	  var items = [];
 
-  $.each(data, function(key, val) {
-    items.push('<li id="' + key + '">' + val + '</li>');
-  });
+	  $.each(data, function(key, val) {
+	    items.push('<li id="' + key + '">' + val + '</li>');
+	  });
 
-  $('<ul/>', {
-    'class': 'my-new-list',
-    html: items.join('')
-  }).appendTo('body');
-});
-)();
+	  $('<ul/>', {
+	    'class': 'my-new-list',
+	    html: items.join('')
+	  }).appendTo('body');
+	});
+
+})();
