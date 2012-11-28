44dde9b2942dbb3a173a2d30d17f5fcc8044d0d1
Ian Gilman <ian@iangilman.com>
2012-11-28 09:11:37 -0800

    Miscellaneous fixes (from Ian)

--- js/js.js
+++ js/js.js
@@ -1,15 +1,17 @@
 (function() {
-	$.getJSON('lists/test.json', function(data) {
-	  var items = [];
-
-	  $.each(data, function(key, val) {
-	    items.push('<li id="' + key + '">' + val + '</li>');
-	  });
-
-	  $('<ul/>', {
-	    'class': 'my-new-list',
-	    html: items.join('')
-	  }).appendTo('body');
-	});
-
+  $(document).ready(function() {
+  	$.getJSON('lists/test.json', function(data) {
+  	  var items = [];
+  
+  	  $.each(data.songs, function(key, val) {
+  	    items.push('<li id="' + key + '">' + val.artist + '</li>');
+  	  });
+  
+  	  $('<ul/>', {
+  	    'class': 'my-new-list',
+  	    html: items.join('')
+  	  }).appendTo('body');
+  	});
+  });
+  
 })();
