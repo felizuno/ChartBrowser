77f41da0123449f83d8c2fba9fbc4159dfd8b0db
Kevin Ewing <notmyuwmail@gmail.com>
2012-11-29 19:02:39 -0800

    Now loads full track ingo

--- js/js.js
+++ js/js.js
@@ -1,15 +1,18 @@
 (function() {
-	$.getJSON('lists/test.json', function(data) {
-	  var items = [];
-
-	  $.each(data, function(key, val) {
-	    items.push('<li id="' + key + '">' + val + '</li>');
-	  });
-
-	  $('<ul/>', {
-	    'class': 'my-new-list',
-	    html: items.join('')
-	  }).appendTo('body');
-	});
-
+  $(document).ready(function() {
+  	$.getJSON('lists/test.json', function(data) {
+  	  var items = [];
+  
+  	  $.each(data.songs, function(key, val) {
+        var track =  val.artist + " - " + val.song + " - From: " + val.album; 
+  	    items.push('<li id="' + key + '">' + track + '</li>');
+  	  });
+  
+  	  $('<ul/>', {
+  	    'class': 'my-new-list',
+  	    html: items.join('')
+  	  }).appendTo('body');
+  	});
+  });
+  
 })();
