5ea7611579b06c3dd93e3bb0cbabb85596b1e502
Kevin Ewing <notmyuwmail@gmail.com>
2012-12-01 08:16:43 -0800

    Button event binding for authentication

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@
     // GLOBAL VARIABLES.
     userName: '',
     currentList: {},
-    
+
 
     // +++++++++++++++++++
     init: function() {
@@ -12,10 +12,17 @@
 
       R.on('change:ready', function(){
         if (R.authenticated()) {
-          this.userName = R.currentUser.attributes.vanityName;
-          $(authbutton).attr('value', 'Logged in as: ' + this.userName);
+          self.userName = R.currentUser.attributes.vanityName;
+          
+          $(authbutton).attr('value', 'Logged in as: ' + self.userName);
+          $(authbutton).bind('click', function() {
+            window.open("http://www.rdio.com/people/"+ self.userName);
+          });
         } else {
           $(authbutton).attr('value', 'Click to authenticate');
+          $(authbutton).bind('click', function() {
+            R.authenticate();
+          });          
         }
       });
     },
