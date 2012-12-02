d084732a974a50b65dd7e6b7d3843a564bd4baa1
Kevin Ewing <notmyuwmail@gmail.com>
2012-12-02 14:05:12 -0800

    Buggy semi-working buttons

--- js/js.js
+++ js/js.js
@@ -9,21 +9,38 @@
     // +++++++++++++++++++
     init: function() {
       var self = this;
+      // initialize the auth button based on current auth state
+      
+      var _stripAuth = function() {
+        self.userName = R.currentUser.attributes.vanityName;
+        
+        $(authbutton).attr('value', 'Logged in as: ' + self.userName);
+        $(authbutton).bind('click', function() {
+          window.open("http://www.rdio.com/people/"+ self.userName);
+        });        
+      };
 
       R.on('change:ready', function(){
         if (R.authenticated()) {
-          self.userName = R.currentUser.attributes.vanityName;
-          
-          $(authbutton).attr('value', 'Logged in as: ' + self.userName);
-          $(authbutton).bind('click', function() {
-            window.open("http://www.rdio.com/people/"+ self.userName);
-          });
+          _stripAuth();
         } else {
           $(authbutton).attr('value', 'Click to authenticate');
           $(authbutton).bind('click', function() {
-            R.authenticate();
+            R.authenticate(kexprdio._stripAuth());
           });          
         }
+      });
+
+      $("#addbutton").click(function () {
+        kexprdio.listLoader.appendToLists();
+      });
+
+      $("#replacebutton").click(function () {
+        kexprdio.listLoader.replaceLists();
+      });
+
+      $("#clearbutton").click(function () {
+        kexprdio.listLoader.clearLists();
       });
     },
 
@@ -32,6 +49,7 @@
   // +++++++++++++++++++
   $(document).ready(function() {
     kexprdio.init();
+    kexprdio.listLoader.init();
   });
 
 })();
--- js/listloader.js
+++ js/listloader.js
@@ -1,22 +1,78 @@
 (function() {
-  // For now all this does is load the test JSON
-  $(document).ready(function() {
-  	$.getJSON('lists/test.json', function(data) {
-  	  var items = [];
   
-  	  $.each(data.songs, function(key, val) {
-        var track =  val.artist + " - " + val.song + " - From: " + val.album; 
-  	    items.push('<li id="' + key + '" class="song">' + track + '</li>');
-  	  });
-  
-  	  $('<ul/>', {
-  	    'class': 'tracklist',
-  	    html: items.join('')
-  	  }).appendTo('.right');
-      // this doesn't work, idk why...
-      $(".tracklist ul li:last-child").addClass("last");
-    });
+  kexprdio.listLoader = {
+    // Rife with self/this issues. Using the blunt "kexprdio.listloader.foo" for the sake of testing 
+    currentList: [], // Arrays right now but need to be objects with a name and an array
+    nextList: [], // I think it's possible to do all of this without nextList, but I am using it to help with _loadFromStorage
 
-  });
+
+    init: function() {
+      // TEMPORARY: loads test JSON immediately. 
+      // setTimeout used because it appends an empty <ul> otherwise. :-( 
+      this._loadFromStorage('test');
+      setTimeout('kexprdio.listLoader._attach(kexprdio.listLoader.currentList)', 300);
+    },
+
+    // local utility loading behaviors
+    //
+    //-----
+    _loadFromStorage: function(listToLoad) {
+      //
+      kexprdio.listLoader.nextList = [];
+      $.getJSON('lists/' + listToLoad + '.json', function(data) {
+    
+        $.each(data.songs, function(key, val) {
+          var track =  val.artist + " - " + val.song + " - From: " + val.album; 
+          kexprdio.listLoader.nextList.push('<li id="' + key + '" class="song">' + track + '</li>');
+        });
+        console.log('Just loaded new nextList:' + kexprdio.listLoader.nextList);
+      });
+
+      kexprdio.listLoader.currentList = kexprdio.listLoader.nextList.slice();
+    },
+    //-----
+    _clearLists: function() {
+      $('.tracklist').remove();
+    },
+    //-----
+    //this should already work
+    _attach: function(listToAttach) {
+      $('<ul/>', {
+      'class': 'tracklist',
+      html: listToAttach.join('')
+      }).appendTo('.right');
+
+      // this doesn't work, idk why...
+      $('.tracklist ul:last-child').addClass('last');
+    },
+    // available commands
+    //
+    //+++++++++
+    clearLists: function() {
+      this._clearLists();
+    },
+    //+++++++++    
+    appendToLists: function() {
+      var listName = 'top100rap'; // Temporarily assigned to one of the test lists
+      this._loadFromStorage(listName);
+      setTimeout('kexprdio.listLoader._attach(kexprdio.listLoader.currentList)', 300);
+//      this._attach(listName); // Want to be doing this instead of the setTimeout
+    },
+    //+++++++++
+    replaceLists: function() {
+      var listName = 'streetsounds_092112'; // Temporarily assigned to one of the test lists
+      this._loadFromStorage(listName);
+      
+      if (this.nextList[0]) {
+        // Want these to be listloader's master variables. this/self cleans this up but IDK how to apply it yet.
+        kexprdio.listLoader.currentList = kexprdio.listLoader.nextList.slice(); 
+        kexprdio.listLoader.nextList = [];
+      }
+
+      this._clearLists();
+      this._attach(kexprdio.listLoader.currentList);
+    }
+
+  };
 
 })();
