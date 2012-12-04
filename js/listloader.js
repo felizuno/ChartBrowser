2e8d210fecc8f6098700a0d58a57f23a33555fff
Kevin Ewing <notmyuwmail@gmail.com>
2012-12-03 19:26:25 -0800

    Now generating list headers from JSON

--- js/listloader.js
+++ js/listloader.js
@@ -1,57 +1,26 @@
 (function() {
   
   kexprdio.listLoader = {
-    // Rife with self/this issues. Using the blunt "kexprdio.listloader.foo" for the sake of testing 
-    currentList: [], // Arrays right now but need to be objects with a name and an array
-    nextList: [], // I think it's possible to do all of this without nextList, but I am using it to help with _loadFromStorage
-
 
     init: function() {
       // TEMPORARY: loads test JSON immediately. 
-      // setTimeout used because it appends an empty <ul> otherwise. :-( 
       this._loadFromStorage('test');
     },
 
     // local utility loading behaviors
     //
-    //-----
-    _loadFromStorage: function(listToLoad) {
-      var self = this;
-      this.nextList = [];
-      var newList = {};
-      $.getJSON('lists/' + listToLoad + '.json', function(data) {
-        $.each(data.songs, function(key, val) {
-          var rdioQuery = val. artist + ' ' + val.song;
-          var track =  val.artist + " - " + val.song + " - From: " + val.album;
-          var trackImageUrl = '';
-
-          self.nextList.push('<li class="song" data-rdioquery="' 
-            + rdioQuery 
-            + '"><img src="'
-            + trackImageUrl 
-            + '" />' 
-            + track 
-            + '</li>');
-        });
-        // build newList and pass it to attach, and delete currentList and nextList
-        self.currentList = self.nextList.slice();
-        self._attach(self.currentList);
-      });
-    },
-    //-----
-    _clearLists: function() {
-      console.log('Clearing list(s)')
-      $('.tracklist').remove();
-    },
-    //-----
     _attach: function(listToAttach) {
-      console.log('About to attach: ' + listToAttach);
+      var $listTitle = $('<div />', {
+        'class': 'list-header',
+        html: listToAttach.title
+      }).appendTo('.right');
+
       var $ul = $('<ul/>', 
         {'class': 'tracklist',
-          html: listToAttach.join('')
+          html: listToAttach.list.join('')
         }
        ).appendTo('.right');
-      // this doesn't work, idk why...
+
       $('ul.tracklist li:last-child').addClass('last');
       
       R.ready(function() {
@@ -66,13 +35,44 @@
               $li.find('img').attr('src', trackImageUrl);
             },
             error: function() {
-              //load placeholder album art for "Track not available on Rdio"
+              // Needs to load placeholder album art for "Track not available on Rdio"
               console.log('R.request failed for search term:' + rdioQuery);
             }
           });
         });
       });
     },
+    //-----
+    _loadFromStorage: function(listToLoad) {
+      var self = this;
+      var newList = {
+        title: '',
+        list: []
+      };
+
+      $.getJSON('lists/' + listToLoad + '.json', function(data) {
+        newList.title = data.name;
+
+        $.each(data.songs, function(key, val) {
+          var rdioQuery = val. artist + ' ' + val.song;
+          var track =  val.artist + " - " + val.song;
+
+          newList.list.push('<li class="song" data-rdioquery="' 
+            + rdioQuery 
+            + '"><img />' 
+            + track 
+            + '</li>');
+        });
+
+        self._attach(newList);
+      });
+    },
+    //-----
+    _clearLists: function() {
+      $('.tracklist').remove();
+      $('.list-header').remove();
+    },
+    //-----
     
     // available commands
     //
